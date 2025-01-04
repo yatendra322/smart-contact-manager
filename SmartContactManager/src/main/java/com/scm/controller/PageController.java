5318d85c20e553a8725161edad18f3d69e1840ba
yatendra-agnisys <yatendra@agnisys.com>
2025-01-04 16:20:10 +0530

    Template Understanding ...

--- SmartContactManager/src/main/java/com/scm/controller/PageController.java
+++ SmartContactManager/src/main/java/com/scm/controller/PageController.java
@@ -1,15 +1,35 @@
 package com.scm.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PageController {
-
     @RequestMapping("/home")
-    public String home() {
+    public String home(Model model) {
         System.out.println("home page handler ");
+        model.addAttribute("name", "yatendra");
+        model.addAttribute("youtubeChannel", "youtube channel");
+        model.addAttribute("githubRepo", "Github channel");
         return "home";
+    }
+
+    // about route
+    @RequestMapping("/about")
+    public String requestMethodName(Model model) {
+        model.addAttribute("isLogin", false);
+        System.out.println("About page loading ...");
+        return "about";
+    }
+
+    // servicess
+    @RequestMapping("/services")
+    public String servicePage() {
+        System.out.println("Service page loading ... ");
+        return "services";
     }
 
 }
