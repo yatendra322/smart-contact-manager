8c4228d1a48fc3cd297ffe210caf7711f09d6b0e
Shivam-Raghav <143813285+Shivam-Raghav@users.noreply.github.com>
2025-01-06 23:58:14 +0530

    project set
    tailwind setup

--- SmartContactManager/tailwind.config.js
+++ SmartContactManager/tailwind.config.js
@@ -1,10 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [ "./src/main/resources/**/*.{html,js}"],
+module.exports = {
+  content: [],
   theme: {
     extend: {},
   },
   plugins: [],
-  darkMode: "class",
 }
 
